d6649163fd3b6af69f9bc68fa7df55a7259aeec5
abi1395 <103116460+abi1395@users.noreply.github.com>
2023-03-17 21:17:18 +0530

    Add files via upload

---
+++ Assignment operators.py
@@ -0,0 +1,30 @@
+a=20
+b=20
+
+# "+=" operator
+a+=b
+print(a)
+
+# "-="
+a-=b
+print(a)
+
+# "*="
+a*=b
+print(a)
+
+# "/="
+a/=b
+print(a)
+
+# "%="
+a%=b
+print(a)
+
+# "//"
+a//=b
+print(a)
+
+#"**="
+a**=b
+print(a)

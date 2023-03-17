15d02c7acb46e435aa03a105155c0387d5d2e595
abi1395 <103116460+abi1395@users.noreply.github.com>
2023-03-17 21:11:35 +0530

    Add files via upload

---
+++ Arithmetic operators.py
@@ -0,0 +1,22 @@
+a=10
+b=5
+#additon
+print(a+b)
+
+#subtraction
+print(a-b)
+
+#multiplication
+print(a*b)
+
+#division
+print(a/b)
+
+#modulus
+print(a%b)
+
+#exponentiation
+print(a**b)
+
+#floor division
+print(a//b)

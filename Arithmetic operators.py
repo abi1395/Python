68c40fc32926c12d1d22635967d818adf897ccf5
Padma Abinaya <103116460+abi1395@users.noreply.github.com>
2023-11-07 16:23:34 +0400

    Update Arithmetic operators.py

--- Arithmetic operators.py
+++ Arithmetic operators.py
@@ -20,3 +20,21 @@ print(a**b)
 
 #floor division
 print(a//b)
+
+#examples 
+print(2+3)
+5
+print(3-2)
+1
+print(2*3)
+6
+print(3/2)
+1.5
+print(2/3)
+0.6666666666666666
+print(2**3)
+8
+print(2%3)
+2
+print(3//2)
+1
